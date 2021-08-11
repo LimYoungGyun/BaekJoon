3d5a283d8938b94db06363eaa4aa93f2fb7fdd5d
DESKTOP-KFM93GG\user <iyg1218@gmail.com>
2021-08-11 23:36:17 +0900

    초기 설정및 Quiz_02675

---
+++ src/step01/Quiz_02675.java
@@ -0,0 +1,25 @@
+package step01;
+
+import java.util.Scanner;
+
+public class Quiz_02675 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int cnt = scan.nextInt();
+		
+		for (int k = 0; k < cnt; k++) {
+			int R = scan.nextInt(); // 입력된 반복 횟수
+			String S = scan.next(); // 입력된 문자열
+			
+			for (int i = 0; i < S.length(); i++) {
+				for (int j = 0; j < R; j++) {
+					System.out.print(S.charAt(i));
+				}
+			}
+			System.out.println();
+		}
+		scan.close();
+	}
+}
