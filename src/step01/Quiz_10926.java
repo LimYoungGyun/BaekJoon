55e5c27dd7ed0efdd10d1c5f2688ac3ed639488a
iyg1218 <iyg1218@gmail.com>
2022-02-17 23:32:53 +0900

    package step01 upload

---
+++ src/step01/Quiz_10926.java
@@ -0,0 +1,12 @@
+package step01;
+
+import java.util.Scanner;
+
+public class Quiz_10926 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String id = scan.nextLine();
+		System.out.println(id + "??!");
+	}
+}
---
+++ src/step01/Quiz_18108.java
@@ -0,0 +1,18 @@
+package step01;
+
+import java.util.Scanner;
+
+public class Quiz_18108 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int year = scan.nextInt();
+		
+		System.out.println(year - 543);
+		
+		scan.close();
+	}
+
+}
