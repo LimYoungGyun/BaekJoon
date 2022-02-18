2eb298b663f87b6bc0f02e8fbf156d6f5cd8b6ca
iyg1218 <iyg1218@gmail.com>
2022-02-18 23:46:44 +0900

    package step03 upload

--- src/step03/Quiz_02742.java
+++ src/step03/Quiz_02742.java
@@ -1,19 +1,27 @@
 package step03;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Quiz_02742 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		Scanner scan = new Scanner(System.in);
-		int number = scan.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		for (int i = number; i > 0; i--) {
-			System.out.println(i);
+		int num = Integer.valueOf(br.readLine());
+		br.close();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		for (int i = num; i > 0; i--) {
+			bw.write(i + "\n");
 		}
 		
-		scan.close();
+		bw.flush();
+		bw.close();
 
 	}
 
