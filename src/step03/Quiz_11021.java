7662f3b316b89f3fd6499d66e6331c69ef249ba3
iyg1218 <iyg1218@gmail.com>
2022-02-19 00:00:15 +0900

    package step03 upload

--- src/step03/Quiz_11021.java
+++ src/step03/Quiz_11021.java
@@ -1,25 +1,29 @@
 package step03;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Quiz_11021 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		Scanner scan = new Scanner(System.in);
-		int number = scan.nextInt();
-		int result = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		for (int i = 1; i <= number; i++) {
-			int num1 = scan.nextInt();
-			int num2 = scan.nextInt();
-			
-			result = num1 + num2;
-			
-			System.out.println("Case #" + i + ": " + result);
+		int count = Integer.valueOf(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+		for (int i = 1; i <= count; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			bw.write("Case #" + i + ": " + (Integer.valueOf(st.nextToken()) + Integer.valueOf(st.nextToken())) + "\n");
 		}
-		
-		scan.close();
+		br.close();
+		bw.flush();
+		bw.close();
 	}
-
 }
