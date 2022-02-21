586db3fd9638c3648d2dfe680bd5a1fd60e3413a
iyg1218 <iyg1218@gmail.com>
2022-02-22 00:30:08 +0900

    package step05 upload

--- src/step05/Quiz_02562.java
+++ src/step05/Quiz_02562.java
@@ -1,32 +1,37 @@
 package step05;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Quiz_02562 {
-
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
-		int[] number = new int[9];
-
-		for (int i = 0; i < number.length; i++) {
-			number[i] = scan.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] arr = new int[9];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.valueOf(br.readLine());
 		}
-
+		
 		int max = 0;
 		int index = 0;
-		for (int i = 0; i < number.length; i++) {
-			if (max < number[i]) {
-				max = number[i];
-				index = i + 1;
+		int cnt = 0;
+		
+		for (int val : arr) {
+			cnt++;
+			if (val > max) {
+				max = val;
+				index = cnt;
 			}
+			
 		}
-
-		System.out.println(max);
-		System.out.println(index);
-
-		scan.close();
-
+		bw.write(max + "\n" + index);
+		br.close();
+		bw.flush();
+		bw.close();
 	}
-
 }
--- src/step05/Quiz_10818.java
+++ src/step05/Quiz_10818.java
@@ -1,35 +1,34 @@
 package step05;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Quiz_10818 {
-
-	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int number = scan.nextInt();
-		int[] arr = new int[number];
+		int num = Integer.valueOf(br.readLine());
 		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = scan.nextInt();
+		int[] arr = new int[num];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int i = 0;
+		while (st.hasMoreTokens()) {
+			arr[i] = Integer.valueOf(st.nextToken());
+			i++;
 		}
 		
-		int max = arr[0];
-		int min = arr[0];
-		for (int i = 0; i < arr.length; i++) {
-			if (max < arr[i]) {
-				max = arr[i];
-			}
-			if (min > arr[i]) {
-				min = arr[i];
-			}
-		}
+ 		Arrays.sort(arr);
 		
-		System.out.print(min + " " + max);
-		
-		scan.close();
-
+		bw.write(arr[0] + " " + arr[num - 1]);
+		br.close();
+		bw.flush();
+		bw.close();
 	}
-
 }
