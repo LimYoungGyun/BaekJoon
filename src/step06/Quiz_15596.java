b087be8a887fc736263287d717bde95f2d00ad69
iyg1218 <iyg1218@gmail.com>
2022-03-03 00:26:56 +0900

    package step06 upload

---
+++ src/step06/Quiz_15596.java
@@ -0,0 +1,13 @@
+package step06;
+
+import java.io.IOException;
+
+public class Quiz_15596 {
+	public static void main(String[] args) throws IOException {
+		Quiz_15596_Test test = new Quiz_15596_Test();
+		
+		int[] a = new int[4];
+		
+		test.sum(a);
+	}
+}
---
+++ src/step06/Quiz_15596_Test.java
@@ -0,0 +1,12 @@
+package step06;
+
+public class Quiz_15596_Test {
+	long sum(int[] a) {
+		int sum = 0;
+		
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		return sum;
+	}
+}
