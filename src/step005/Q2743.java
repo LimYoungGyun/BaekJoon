ccccd9142e80efa8387ba8fa6e793f489b4960d6
iyg1218 <iyg1218@gmail.com>
2024-12-11 00:14:32 +0900

    FEAT : 5단계 문자열 #2743
    #2743 단어 길이 재기

---
+++ src/step005/Q2743.java
@@ -0,0 +1,17 @@
+package step005;
+
+import java.util.Scanner;
+
+/**
+ * @author : lim
+ * @description : "package" 라인 제외, 출제 번호를 Main으로 변경 후 제출. (채점이 진행되지 않음)
+ * @since : 24. 12. 11.(수) 00:11
+ */
+public class Q2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String N = sc.next();
+
+		System.out.println(N.length());
+	}
+}
